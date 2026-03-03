4de8c8a944e913894213b8883d470a18e911ec60
Philippe Bovey <philippe.bovey@eduvaud.ch>
2026-03-03 09:09:52 +0100

    Paquage NuGet - demo Serial Port

---
+++ DemoJava/demo25_26/src/demo25_26/demo.java
@@ -0,0 +1,30 @@
+package demo25_26;
+
+public class demo 
+{
+
+	public static void main(String[] args) 
+	{
+		//-- types primitif--// 
+		//-- entier --
+		byte					//-> 1 octet 
+		short 					//-> 2 octets 
+		int						//-> 4 octets 
+		long					//-> 8 octets 
+		
+		//-- réel --
+		float					//-> 4 octets 
+		double 					//-> 8 octets
+		
+		//-- autre --// 
+		boolean
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
